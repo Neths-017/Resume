21dfcd13b21dbe8939c89d00e43a0c4479c4adcb
Neths-017 <trajanomarckenneth625@gmail.com>
2025-02-01 11:25:21 +0800

    fix Email sender

--- scripts/EmailSender.js
+++ scripts/EmailSender.js
@@ -1,4 +1,6 @@
 function sendEmail() {
+  const serviceID = "service_q8ex3fc";
+  const templateID = "template_zt40ugjn";
   const parms = {
     nametext: document.getElementById("nametext").value,
     Sub_text: document.getElementById("Sub_text").value,
@@ -7,7 +9,12 @@ function sendEmail() {
     Message_text: document.getElementById("Message_text").value,
   };
 
-  emailjs
-    .send("service_biizjvy", "template_ft2f7j5", parms)
-    .then(alert("Sent!"));
+  emailjs.send(serviceID, templateID, parms).then(
+    (response) => {
+      console.log("SUCCESS!", response.status, response.text);
+    },
+    (error) => {
+      console.log("FAILED...", error);
+    }
+  );
 }
