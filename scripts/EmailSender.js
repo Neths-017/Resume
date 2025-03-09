a52a309ff78ca328f1d50063f8649e64c0430c61
Neths-017 <trajanomarckenneth625@gmail.com>
2025-03-09 14:30:57 +0800

    remove contact forms form

--- scripts/EmailSender.js
+++ scripts/EmailSender.js
@@ -1,20 +1,4 @@
-function sendEmail() {
-  const serviceID = "service_q8ex3fc";
-  const templateID = "template_zt40ugjn";
-  const parms = {
-    nametext: document.getElementById("nametext").value,
-    Sub_text: document.getElementById("Sub_text").value,
-    Customer_email: document.getElementById("Customer_email").value,
-    mobile: document.getElementById("mobile").value,
-    Message_text: document.getElementById("Message_text").value,
-  };
-
-  emailjs.send(serviceID, templateID, parms).then(
-    (response) => {
-      console.log("SUCCESS!", response.status, response.text);
-    },
-    (error) => {
-      console.log("FAILED...", error);
-    }
-  );
+function SendEmail(){
+  alert("Contact form not working for the moment");
 }
+
